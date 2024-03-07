b0d4b263db07e754420f47e95fa8757d83d860b7
ajamwal13 <adit.jamwal@gmail.com>
2024-03-07 16:18:05 -0500

    Final commit

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.header('Tossing a Coin')
+st.header('This is the data on vehicles in the U.S.')
 
-st.write('It is not a functional application yet. Under construction.')
+data = pd.read_csv('../vehicles_us.csv')
+
+agree = st.checkbox('Display histogram on odometer readings')
+
+if agree:
+    fig3 = px.histogram(data, x='odometer', title='Vehicle Odometer Distribution')
+    st.write(fig3)
+
+fig = px.histogram(data, x='price', title='Vehicle Price Distribution')
+st.write(fig)
+
+fig2 = px.scatter(data, x='price', y='days_listed', title='Vehicle Odometer Distribution')
+st.write(fig2)
 
