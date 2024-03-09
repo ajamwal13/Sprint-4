388d27aa72fe2ea73e330d641ea768563f914c59
ajamwal13 <adit.jamwal@gmail.com>
2024-03-09 10:58:23 -0500

    Revision

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ st.header('This is the data on vehicles in the U.S.')
 
 data = pd.read_csv('vehicles_us.csv')
 
-agree = st.checkbox('Display histogram on odometer readings')
+fig1_check = st.checkbox('Display histogram on vehicle prices')
+fig2_check = st.checkbox('Display scatter plot of days listed versus vehicle price')
+fig3_check = st.checkbox('Display histogram on odometer readings')
 
-if agree:
+if fig1_check:
+    fig = px.histogram(data, x='price', title='Vehicle Price Distribution')
+    st.plotly_chart(fig)
+
+if fig2_check:
+    fig2 = px.scatter(data, x='price', y='days_listed', title='Vehicle Odometer Distribution')
+    st.plotly_chart(fig2)
+
+if fig3_check:
     fig3 = px.histogram(data, x='odometer', title='Vehicle Odometer Distribution')
-    st.write(fig3)
-
-fig = px.histogram(data, x='price', title='Vehicle Price Distribution')
-st.write(fig)
-
-fig2 = px.scatter(data, x='price', y='days_listed', title='Vehicle Odometer Distribution')
-st.write(fig2)
-
+    st.plotly_chart(fig3)
